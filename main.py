db52f20f9116cd8c03ba132eafcc973c396663c6
Daniel Medina <daniel@medina.com>
2024-10-10 17:19:50 -0600

    Added some comments including documentation and possible additions.

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import os
 from word_list import words
 from utils import (
-    fits_horizontally,
-    fits_vertically,
-    random_coordinate,
-    random_word,
     fill_board_with_words,
     fill_remainig_spaces,
     print_board,
@@ -31,8 +27,15 @@ def main():
     print("===== WORD SEARCH =====")
     print_board(board)
 
-    for i in words_in_board:
-        print(f"{i['word']} is at {i['coord']} in direction {i['direction']}")
+    # Prints words, their position, and direction.
+    # for i in words_in_board:
+    #     print(f"{i['word']} is at {i['coord']} in direction {i['direction']}")
+
+    # * Next steps are to allow user to input the coordinates of the word they've found and check if it's correct and in the words_in_board list
+    # * If it is, then the word will be marked as found and the board will be printed again
+    # * If all words are found, then the game will end
+
+    # ? Possible additions: Include a timer with a cloud-based leaderboard to track the fastest times
 
 
 if __name__ == "__main__":
